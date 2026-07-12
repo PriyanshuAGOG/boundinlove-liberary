c6473cf855ee388b5c87eb9879ffada84496826b
PriyanshuAGOG <chat.priyanshuag@gmail.com>
2026-07-12 10:07:56 +0530

    Declare Cloudflare worker globals

--- types/cloudflare-workers.d.ts
+++ types/cloudflare-workers.d.ts
@@ -1,6 +1,12 @@
+type Fetcher = {
+  fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+};
+
+type D1Database = any;
+
 declare module "cloudflare:workers" {
   export const env: {
-    DB?: any;
+    DB?: D1Database;
     [binding: string]: any;
   };
 }
